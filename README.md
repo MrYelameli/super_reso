0472479e572c59d8c467d684e3c025c501100edf
Mallikarjun Yelameli <mallikarjun.yelameli@live.com>
2020-05-08 00:27:04 +0900

    added program

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # super_reso
 super resolution 
+
+How to use: 
+python main.py --upscale_factor < > --batchSize < > --testBatchSize 100 --nEpoch < > --lr < >
+
+Ref: https://arxiv.org/abs/1609.05158 
---
+++ main.py
@@ -0,0 +1,100 @@
+#import standards import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models # add models to the list
+from torchvision.utils import make_grid
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+
+
+# ignore harmless warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+#Prepare datasets
+root='/probav_datasets/'
+
+train_data = datasets.ImageFolder(os.path.join(root, 'train'))
+test_data = datasets.ImageFolder(os.path.join(root, 'test'))
+
+torch.manual_seed(42)
+train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=10, shuffle=True)
+
+class_names = train_data.classes
+
+
+#define the network
+class Net(nn.Module):
+    def __init__(self, upscale_factor):
+        super(Net, self).__init__()
+
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.pixel_shuffle(self.conv4(x))
+        return x
+
+    def _initialize_weights(self):
+        init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
+        init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
+        init.orthogonal_(self.conv3.weight, init.calculate_gain('relu'))
+        init.orthogonal_(self.conv4.weight)
+
+model = Net(upscale_factor=opt.upscale_factor).to(device)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+def train(epoch):
+    epoch_loss = 0
+    for iteration, batch in enumerate(training_data_loader, 1):
+        input, target = batch[0].to(device), batch[1].to(device)
+
+        optimizer.zero_grad()
+        loss = criterion(model(input), target)
+        epoch_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+        print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(training_data_loader), loss.item()))
+
+    print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, epoch_loss / len(training_data_loader)))
+
+
+def test():
+    avg_psnr = 0
+    with torch.no_grad():
+        for batch in testing_data_loader:
+            input, target = batch[0].to(device), batch[1].to(device)
+
+            prediction = model(input)
+            mse = criterion(prediction, target)
+            psnr = 10 * log10(1 / mse.item())
+            avg_psnr += psnr
+    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+
+def checkpoint(epoch):
+    model_out_path = "model_epoch_{}.pth".format(epoch)
+    torch.save(model, model_out_path)
+    print("Checkpoint saved to {}".format(model_out_path))
+
+for epoch in range(1, opt.nEpochs + 1):
+    train(epoch)
+    test()
+    checkpoint(epoch)
